742608ee136b22074433e0da1951ef57f2bf430e
Paula Quitián <pquitian@idealista.com>
2018-05-17 21:25:26 +0200

    [webptmad0418]Rulo-Paula

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,34 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    for (var i = cardsArr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = cardsArr[i];
+        cardsArr[i] = cardsArr[j];
+        cardsArr[j] = temp;
+    }
+    return cardsArr;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// MemoryGame.prototype.finished = function () {
+    this.pairsClicked++;
+    if (firstCard === secondCard) {
+        this.pairsGuessed++;
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+    while (this.pairsGuessed < 12) return false;
+    return true;
+};
